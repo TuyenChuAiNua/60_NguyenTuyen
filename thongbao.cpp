deae3cd12f14e2ceb982068b8213c50705683720
TuyenChuAiNua <nguyentuyenn1311@gmail.com>
2024-03-12 13:02:58 +0700

    t them file thong bao moi vao nhanh tuyen-new

--- thongbao.cpp
+++ thongbao.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 using namespace std;
-
-// Ham main() la noi su thuc thi chuong trinh bat dau
-
 int main()
 {
-   cout << "Hello World"; // In dong chu Hello World
+   cout << "Hello World";
    return 0;
 }
